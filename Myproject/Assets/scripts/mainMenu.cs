a2d8c8497fd93f4cef9a2dcdbd957704b9e8de1c
DeppieK <deppie4@gmail.com>
2024-03-11 08:49:36 +0200

    modified menu scene

---
+++ Myproject/Assets/scripts/mainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void PlayEasyGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
