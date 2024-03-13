0bfe2b20bef697f16c5ccae7d1c2a9cd4d18afa3
DeppieK <deppie4@gmail.com>
2024-03-13 11:18:41 +0200

    pick up object impl (WIP)

--- Myproject/Assets/scripts/Bottle.cs
+++ Myproject/Assets/scripts/Bottle.cs
@@ -1,16 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bottle : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
+    [SerializeField] private Animator _animator; // Reference to the Animator component
+
+    // Index of the layer you want to animate
+    [SerializeField] private int _layerIndex = 2;
 
     public string InteractionPrompt => _prompt;
 
     public bool Interact(Interactor interactor)
     {
-        Debug.Log("Take a shot :D");
+        if (_animator != null)
+        {
+            // Trigger the "PickUp" animation in the specified layer
+            _animator.Play("PickUp", _layerIndex);
+        }
+        else
+        {
+            Debug.LogWarning("Animator reference not set in Bottle script.");
+        }
+
         return true;
     }
 }
