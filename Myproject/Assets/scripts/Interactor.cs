da9251668de884988f79fc39453df2b1b6271814
DeppieK <deppie4@gmail.com>
2024-03-13 08:40:33 +0200

    UI panel impl

--- Myproject/Assets/scripts/Interactor.cs
+++ Myproject/Assets/scripts/Interactor.cs
@@ -9,10 +9,14 @@ public class Interactor : MonoBehaviour
     [SerializeField] private Transform _interactionPoint;
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask _interactableMask;
+    [SerializeField] private InteractionPromptUI _interactionPromptUI ;
+
 
     private readonly Collider[] _colliders = new Collider[3];
 
     [SerializeField] private int _numFound;
+
+    private IInteractable _interactable;
 
     private void Update()
     {
@@ -20,19 +24,18 @@ public class Interactor : MonoBehaviour
 
         if (_numFound > 0)
         {
-            Debug.Log("hello num is greater than 0."); // Add this debug log
-
-            var interactable = _colliders[0].GetComponent<IInteractable>();
-            if (interactable != null && Keyboard.current.eKey.wasPressedThisFrame)
+            var _interactable = _colliders[0].GetComponent<IInteractable>();
+            if (_interactable != null)
             {
-                //Debug.Log("E key was pressed this frame."); // Add this debug log
-                interactable.Interact(this);
+                if (!_interactionPromptUI.IsDisplayed) _interactionPromptUI.SetUp(_interactable.InteractionPrompt);
+                if (Keyboard.current.eKey.wasPressedThisFrame) _interactable.Interact(this);
             }
-            else if (interactable == null)
-            {
-                Debug.Log("null :(");
-
-            }
+            
+        }
+        else
+        {
+            if (_interactable != null) _interactable = null;
+            if (_interactionPromptUI.IsDisplayed) _interactionPromptUI.Close();
         }
     }
 
