e610bee131fe13b7b602128d479d053243f34ad8
DeppieK <deppie4@gmail.com>
2024-03-14 23:54:29 +0200

    picking up items impl + inventory canvas

---
+++ Myproject/Assets/scripts/interactableObj.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Bottle : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _prompt;
+    public GameObject player;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public string InteractionPrompt => _prompt;
+
+    public bool Interact(Interactor interactor)
+    {
+        Destroy(gameObject);
+        return true;
+    }
+}
