67d5de847e0e4893c5f32f149fbad63ca7fe287c
Stella Anast <79939214+stella-anast@users.noreply.github.com>
2024-03-16 03:22:12 +0200

    battle part2

--- Myproject/Assets/scripts/Enemy.cs
+++ Myproject/Assets/scripts/Enemy.cs
@@ -1,25 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 
 {
-    [SerializeField] float health;
+    [SerializeField] float health = 3;
+
+
+    [Header("Combat")]
+    [SerializeField] float attackCD = 0f;
+    [SerializeField] float attackRange = 1f;
+    [SerializeField] float aggroRange = 2f;
 
     GameObject player;
+    NavMeshAgent agent;
     Animator animator;
+    float timePassed;
+    float newDestinationCD = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
     }
+    void Update()
+    {
+
+        animator.SetFloat("speed", agent.velocity.magnitude / agent.speed); // Use navMeshAgent
+
+
+        if (timePassed >= attackCD)
+        {
+            if (Vector3.Distance(player.transform.position, transform.position) <= attackRange)
+            {
+                animator.SetTrigger("attack");
+                timePassed = 0;
+            }
+        }
+        timePassed += Time.deltaTime;
+
+        if (newDestinationCD <= 0 && Vector3.Distance(player.transform.position, transform.position) <= aggroRange)
+        {
+            newDestinationCD = 0.5f;
+            agent.SetDestination(player.transform.position);
+        }
+        newDestinationCD -= Time.deltaTime;
+        transform.LookAt(player.transform);
+  
+    }
+
     public void TakeDamage(float damageAmount)
     {
         health -= damageAmount;
         animator.SetTrigger("damage");
-        if(health<=0)
+        
+
+        if (health <= 0)
         {
             Die();
         }
@@ -28,5 +68,12 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         Destroy(this.gameObject);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, aggroRange);
     }
 }
