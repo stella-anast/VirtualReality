73d3755139d9110d9bfb12605e80f75ebd6ff0fb
DeppieK <deppie4@gmail.com>
2024-03-16 12:50:43 +0200

    questManager

---
+++ Myproject/Assets/scripts/QuestManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class QuestManager : MonoBehaviour
+{
+    public static QuestManager Instance { get; set; }
+
+    public List<Quest> allActiveQuests;
+    public List<Quest> allCompletedQuests;
+
+    [Header("QuestMenu")]
+    public GameObject questMenu;
+    public bool isQuestMenuOpen;
+
+    public GameObject activeQuestPrefab;
+    public GameObject completedQuestPrefab;
+
+    public GameObject questMenucontent;
+
+    [Header("QuestTracker")]
+    public GameObject questTrackerContent;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
+
