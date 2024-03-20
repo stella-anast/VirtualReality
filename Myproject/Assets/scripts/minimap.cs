7f9c6b750e7c0cb62150b6d033a10a26202aeaa4
KONSTANTINA PARTHENIA KAPSALI <133678045+Nantinkpsl20@users.noreply.github.com>
2024-03-20 02:18:51 +0200

    movePlayerOnMinimap

--- Myproject/Assets/scripts/minimap.cs
+++ Myproject/Assets/scripts/minimap.cs
@@ -10,9 +10,35 @@ public class minimap : MonoBehaviour
         
     }
 
+    public RectTransform playerInMap;
+    public RectTransform map2dEnd;
+    public Transform map3dParent;
+    public Transform map3dEnd;
+
+    private Vector3 normalized, mapped;
+
     // Update is called once per frame
     void Update()
     {
-        
+        normalized = Divide(
+                 map3dParent.InverseTransformPoint(this.transform.position),
+                 map3dEnd.position - map3dParent.position
+             );
+        normalized.y = normalized.z;
+        mapped = Multiply(normalized, map2dEnd.localPosition);
+        mapped.z = 0;
+        playerInMap.localPosition = mapped;
     }
+    
+
+
+private static Vector3 Divide(Vector3 a, Vector3 b)
+{
+    return new Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
+}
+
+private static Vector3 Multiply(Vector3 a, Vector3 b)
+{
+    return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
+}
 }
